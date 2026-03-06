a17bf4b421f5f7de74c4878909968472371356c9
MatheusRw <mt.richaard@hotmail.com>
2026-03-06 18:31:50 -0300

    testando primeiro codigo

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print('teste')
-print("começando novamente")
+import requests
+
+from bs4 import BeautifulSoup
+
+response = requests.get("https://g1.globo.com/")
+
+content = response.content
+
+site = BeautifulSoup(content, "html.parser")
+
+noticias = site.find_all("div", class_="feed-post-body")
+
+titulo = noticia.find("a", attrs={"class": "feed-post-link"})
+print(titulo.text)
+
+subtitulo = noticia.find("div", attrs={"class": "feed-post-body-resumo"})
+print(subtitulo.text)
