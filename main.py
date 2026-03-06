d8b295156a66e41f72a8a6e6a024d6f9bb0010cc
MatheusRw <mt.richaard@hotmail.com>
2026-03-06 18:37:32 -0300

    web scrapping funcional

--- main.py
+++ main.py
@@ -8,10 +8,23 @@ content = response.content
 
 site = BeautifulSoup(content, "html.parser")
 
-noticias = site.find_all("div", class_="feed-post-body")
+noticia = site.find("div", class_="feed-post-body")
 
-titulo = noticia.find("a", attrs={"class": "feed-post-link"})
-print(titulo.text)
+#titulo = noticias.find("a", attrs={"class": "feed-post-link"})
+#print(titulo.text)
 
-subtitulo = noticia.find("div", attrs={"class": "feed-post-body-resumo"})
-print(subtitulo.text)
+if noticia:
+    # Título
+    titulo = noticia.find("a", class_="feed-post-link")
+    if titulo:
+        print("Título:", titulo.text.strip())
+
+    # Subtítulo
+    subtitulo = noticia.find("div", class_="feed-post-body-resumo")
+    if subtitulo:
+        print("Subtítulo:", subtitulo.text.strip())
+else:
+    print("Nenhuma notícia encontrada.")
+
+
+ 
